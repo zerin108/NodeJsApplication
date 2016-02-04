d26978c02d8d9aab7f1e86436520081e9c7bbd21
zerin108 <zerin108@gmail.com>
2016-02-04 14:11:13 +0600

    Просмотрел первый выпуск скринкаста посвященный созданию чата на node.js. Рассмотрели создание сервера и использование middleware. Middleware - такие обработчики, которые обрабатывают req и res, и если у них нет данных для обработки конкретного случая или просто необходимо, они могут передать работу следующему, написанному в коде приложения middleware, с помощью функции "next()". Все встроенные midleware в express вынесенны разработчиками в отдельные модули с сохранением названий.

---
+++ app.js
@@ -0,0 +1,120 @@
+//подключаем express
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var errHandler = require('errorhandler');
+
+//создаем приложение
+var app = express();
+app.set('port', 3000);
+
+//вешаем http-server
+http.createServer(app).listen(app.get('port'), function(){
+    console.log("Express server listenning on port " + app.get('port'));
+});
+
+//middleware
+app.use(function (req, res, next){
+    if (req.url === '/'){
+        res.end("Hello");
+    } else {
+        next();
+    }
+});
+
+app.use(function (req, res, next){
+    if (req.url === '/test'){
+        res.end("test");
+    } else {
+        next();
+    }
+});
+
+app.use(function (req, res, next){
+    if (req.url === '/forbidden'){
+        next(new Error("wops, denied!"));
+    } else {
+        next();
+    }
+});
+
+app.use(function(req, res){
+   res.send(404, "Page Not Found. Sorry"); 
+});
+
+//если у функции 4 аргумента, то это обработчик ошибок
+app.use(function(err, req, res, next){
+    if(app.get('env') == 'development'){
+        errHandler (err, req, res, next);
+    } else {
+        res.send(500);
+    }
+});
+
+
+/*
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var http = require('http');
+
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.set('port', process.env.PORT || 3000);
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+//app.use(express.favicon());
+app.use(express);
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+*/
+
+
+module.exports = app;
